0d2ca6a7f21a869a946e54b42a259e05074ac286
Ygor Lazaro <ygor@ygorlazaro.com>
2018-03-16 11:56:42 -0300

    Primeiros arquivos

---
+++ site.js
@@ -0,0 +1,8 @@
+let idade = prompt("Digite sua idade");
+let maioridade = 18;
+
+if (idade >= maioridade) {
+    alert("Toma uma cerveja ae");
+} else {
+    alert("Volta pro jardim de infância");
+}
